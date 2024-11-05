d041319d13fb2d2e64cbafaba1ea48b2c0cc15c8
Oziphos <chevalier.c.contact@gmail.com>
2024-11-05 17:15:47 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Initialisation des variables
+        Monopoly game = new Monopoly();
+        // Initialisation du jeu
+        creerJoueurs(game);
+        game.initJeu();
+        game.lancerPartie();
+    }
+
+    private static void creerJoueurs(Monopoly jeu) {
+        // Initialisation des variables
+        Scanner scanner = new Scanner(System.in);
+        int nb_joueurs = 0;
+        String input;
+
+        // Structure d'ajout de joueur
+        System.out.println("Entrez le nom du Joueur " + (nb_joueurs + 1) + " :");
+        input = scanner.nextLine();
+        // Boucle
+        while (!input.equals("exit") && nb_joueurs < 2) {
+            // Traitement de l'élément
+            jeu.creerJoueur(input);
+            // Mise à jour de l'élément
+            nb_joueurs++;
+            System.out.println("Entrez le nom du Joueur " + (nb_joueurs + 1) +(nb_joueurs < 2 ? "" : ": ('exit' pour terminer la création de joueur)"));
+            input = scanner.nextLine();
+        }
     }
 }
